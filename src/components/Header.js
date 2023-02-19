2827e989a14c0ca9c4f153e9834944b46198383c
Khizarshah01 <khizar.shah010@gmail.com>
2023-02-19 16:56:46 +0530

    ADD: Header editi

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,12 +12,33 @@ const Header = (props)=>{
                     <input type="text" placeholder=" Search" />
                 </div>
             </Search>
+            <SearchIcon>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              data-supported-dps="16x16"
+              fill="currentColor"
+              width="16"
+              height="16"
+              focusable="false"
+            >
+              <path d="M14.56 12.44L11.3 9.18a5.51 5.51 0 10-2.12 2.12l3.26 3.26a1.5 1.5 0 102.12-2.12zM3 6.5A3.5 3.5 0 116.5 10 3.5 3.5 0 013 6.5z"></path>
+            </svg>
+            </SearchIcon>
         </Content>
     </Container>
     );
 };
 
-const Search = styled.div``;
+const SearchIcon = styled.div``;
+const Search = styled.div`
+opacity: 1;
+flex-grow: 1;
+position: relative;
+& > div{
+    max-width: 280px;
+}
+`;
 const Container = styled.div`
 background-color: white;
 border-bottom: 1px solid rgba(0, 0, 0, 0.08);
